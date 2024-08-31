3881f107625a1e1a10d74a6771ba0e56d21fce7a
David Owen <david.owen@csuglobal.edu>
2024-08-31 17:10:05 -0500

    Print count

--- PPPart1.cpp
+++ PPPart1.cpp
@@ -3,17 +3,26 @@
 #include <mutex>
 
 void countUp() {
-
+    for(int i = 0; i < 20; i++) {
+        std::cout << i << std::endl;
+    }
 }
 
 void countDown() {
+    for(int j = 20; j != 0; j--) {
+        std::cout << j << std::endl;
+    }
 
-    
 }
 
 
 int main() {
 
+    std::thread t1(countUp);
+    std::thread t2(countDown);
+
+    t1.join();
+    t2.join();
 
     return 0;
 }
